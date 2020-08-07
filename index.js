ea5ce36611836dc9ec8eb931ecf4ddb3a56496a8
Amber Kerawala Torres <amber_kerawala@berkeley.edu>
2020-08-07 17:54:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your solution here!
+const append = ["Milo", "Otis", "Garfield"]; //push 
+
+const prepend = ["Milo", "Otis", "Garfield"]; //unshift 
+
+const removeLast = ["Milo", "Otis", "Garfield"]; //pop 
+
+const removeFirst = ["Milo", "Otis", "Garfield"]; //shift 
+
+append.push("Odie"); 
+prepend.unshift("Odie"); 
+removeLast.pop(); 
+removeFirst.shift(); 
